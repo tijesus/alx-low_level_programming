18172f3d8566891c994a3b82e2f0df1773bdf251
tijesus <emmanueltijesu@gmail.com>
2023-10-06 14:50:17 -0700

    printing single digits seperated by comma and space

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -10,8 +10,10 @@ int main(void)
 	for (num = 0; num < 10; num++)
 	{
 		putchar(num + '0');
-		putchar(',');
-		putchar(' ');
+		if (num < 9)
+		{
+			putchar(',');
+			putchar(' ');
 	}
 	putchar('\n');
 	return (0);
