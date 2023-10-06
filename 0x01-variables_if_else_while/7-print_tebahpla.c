cfb30dcd7a535b5f19e75b7eaa35eb674d8663ea
tijesus <emmanueltijesu@gmail.com>
2023-10-06 12:52:29 -0700

    alphabet in reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char mirror;
+
+	for (mirror = 'z'; mirror >= 'a'; mirror++)
+	{
+		putchar(mirror);
+	}
+	putchar('\n');
+	return (0);
+}
