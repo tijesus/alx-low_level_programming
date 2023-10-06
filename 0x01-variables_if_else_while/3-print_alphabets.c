493b25a10c9db9eb7c0e0e631242d6ddeb47d06d
tijesus <emmanueltijesu@gmail.com>
2023-10-06 08:06:38 -0700

    printing vapital and small letter alphabet

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alphaStart, alphaEnd, alpha;
+
+	alphaStart = 'a';
+	alphaEnd = 'z';
+
+	do {
+		putchar(alpha);
+	} while (alphaStart <= alphaEnd);
+		continue;
+	alphaStart = 'A'
+	alphaEnd = 'Z'
+	do {
+		putchar(alpha);
+	} while (alphaStart <= alphaEnd);
+
+	putchar('\n');
+	return (0);
+}
