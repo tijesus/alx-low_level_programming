e73c29114a4a3fc9b2a4bd7b9bf26a2dcf90575d
tijesus <emmanueltijesu@gmail.com>
2023-10-06 07:27:36 -0700

    Alphabets in small letter

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -7,10 +7,10 @@ int main(void)
 {
 	char alpha;
 
-	for (alpha = a; alpha <= z; alpha++)
+	for (alpha = 'a'; alpha <= 'z'; alpha++)
 	{
 		putchar(alpha);
 	}
-	putchar('\n')
+	putchar('\n');
 	return (0);
 }
