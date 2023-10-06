e846d78029ce42221e5cae72dc64b5ef9f5be275
tijesus <emmanueltijesu@gmail.com>
2023-10-06 12:08:00 -0700

    Print numbers less than 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio>
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int number;
+
+	for (number = 0; number < 10; number++)
+	{
+		putchar(number);
+	}
+	putchar('\n');
+	return (0);
+}
