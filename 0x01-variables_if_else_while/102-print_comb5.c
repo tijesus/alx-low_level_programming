0b9b369b38fb39b3f27e4200b2d2deb2d3d9910d
tijesus <emmanueltijesu@gmail.com>
2023-10-06 17:09:59 -0700

    printing all doble digit without exception

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -7,16 +7,16 @@ int main(void)
 {
 	int digit0, digit1;
 
-	for (digit0 = 0; digit0 <= 9; digit0++)
+	for (digit0 = 0; digit0 <= 99; digit0++)
 	{
-		for (digit1 = digit0 + 0; digit1 <= 9; digit1++)
+		for (digit1 = digit0 + 1; digit1 <= 99; digit1++)
 		{
 			putchar((digit0 / 10) + '0');
 			putchar((digit0 % 10) + '0');
 			putchar(' ');
 			putchar((digit1 / 10) + '0');
 			putchar((digit1 % 10) + '0');
-					if (!(digit0 == 9 && digit1 == 9))
+					if (!(digit0 == 98 && digit1 == 99))
 					{
 						putchar(',');
 						putchar(' ');
