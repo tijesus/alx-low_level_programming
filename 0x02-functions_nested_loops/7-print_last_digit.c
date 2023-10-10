07399489a1767e375f9be61b2e3f8e0d1fbed5b8
tijesus <emmanueltijesu@gmail.com>
2023-10-10 15:05:19 -0700

    print last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * print_last_digit - print last digit
+ * @lastDigit: variable argu
+ * Return: 0 success
+ */
+int print_last_digit(int lastDigit)
+{
+	_putchar(lastDigit % 10);
+	return (0);
+
+}
