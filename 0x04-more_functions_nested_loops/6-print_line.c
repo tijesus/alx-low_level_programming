5977e35e4216ed1cbfbf988394c239db0dd56542
tijesus <emmanueltijesu@gmail.com>
2023-10-12 12:33:14 -0700

    print line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_line - print line
+ * @n: variable parameteer
+ */
+void print_line(int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		_putchar('_');
+	}
+	_putchar('\n');
+}
