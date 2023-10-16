6eef1b82bd734b2dd26e37bfe734e64e8212da06
tijesus <emmanueltijesu@gmail.com>
2023-10-16 08:50:53 -0700

    print strings with new line

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+  * _puts - prints output in a newline
+  * @str: pointer
+  * Return: Always 0.
+  */
+void _puts(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+		_putchar(str[i]);
+	_putchar('\n');
+}
