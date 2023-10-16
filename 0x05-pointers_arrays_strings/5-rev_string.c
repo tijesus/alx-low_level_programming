468c786266134ff6d9f9b23927b49cdc8423d473
tijesus <emmanueltijesu@gmail.com>
2023-10-16 11:47:41 -0700

    reversing string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * rev_string - printing string in reverse
+ * @s: pointer to s
+ */
+void rev_string(char *s)
+{
+	int i, j;
+	char hold;
+	int count = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+		count++;
+
+	for (i = 0 , j = count - 1; i < j; i++, j--)
+	{
+		hold = s[i];
+		s[i] = s[j];
+		s[j] = hold;
+	}
+}
