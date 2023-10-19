f1f5e82c340490eb4332d28d9b12a3ea9dd3f3b1
tijesus <emmanueltijesu@gmail.com>
2023-10-19 08:53:01 -0700

    capitalize first word

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * cap_string - capitalise the first letter of a word
+ * @a: pointer to arrar
+ * Return: void
+ */
+char *cap_string(char *a)
+{
+	int i, j;
+	char sep[] = " \t\n,;.!?\"(){}";
+
+	for (i = 0; a[i] != '\0'; i++)
+	{
+		for (j = 0; sep[j] != '\0'; j++)
+		{
+			if (a[i] == sep[j])
+			{
+				if (a[i + 1] >= 'a' && a[i + 1] <= 'z')
+					a[i + 1] = a[i + 1] - 32;
+			}
+		}
+	}
+	return (a);
+}
