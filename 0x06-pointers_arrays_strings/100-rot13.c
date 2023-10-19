81a973d3b44dcdb893b2b0f16dfbdc7981c4d309
tijesus <emmanueltijesu@gmail.com>
2023-10-19 12:38:04 -0700

    rot13 encoding

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * *rot13 - rot13 encoding
+ * @a: pointer parameter
+ * Return: char
+ */
+char *rot13(char *a)
+{
+	int i, j;
+	char k[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char l[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	for (i = 0; a[i] != '\0'; i++)
+	{
+		for (j = 0; k[j] != '\0'; j++)
+			if (a[i] == k[j])
+				a[i] = l[j];
+	}
+	return (a);
+}
