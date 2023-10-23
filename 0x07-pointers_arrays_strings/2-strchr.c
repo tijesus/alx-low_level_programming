ed6dac31466dee1ba4516fa7e655981a600c60a3
tijesus <emmanueltijesu@gmail.com>
2023-10-23 13:32:53 -0700

    function to locate character in a string

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -8,14 +8,14 @@
  */
 char *_strchr(char *s, char c)
 {
-	int i;
+	unsigned int i;
 
-	for (i = 0; s[i] != '\0'; i++)
+	for (i = 0; ; i++)
 	{
 		if (s[i] == c)
-			return (s + i);
+			return (&s[i]);
+
 		else if (s[i] == '\0')
 			return (0);
 	}
-	return (0);
 }
