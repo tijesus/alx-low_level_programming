6cb22de58f4deed4e3192d1e3a3e7ec00b2cb378
tijesus <emmanueltijesu@gmail.com>
2023-10-23 10:29:46 -0700

    looking for needle in a haystack

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * _strstr - locating a substring
+ * @haystack: pointer to full strings
+ * @needle: pointer to value to remove from the string
+ * Return: char
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	int i, j;
+
+	for (i = 0; haystack[i] != '\0'; i++)
+	{
+		for (j = 0; needle[j] != '\0'; j++)
+		{
+			if (haystack[i + j] != needle[j])
+			{
+				break;
+			}
+		}
+		if (needle[j] == '\0')
+		{
+			return (haystack + i);
+		}
+	}
+	return (0);
+}
