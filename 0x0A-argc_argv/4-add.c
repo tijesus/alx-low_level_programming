397d76b16898bcfa5b19fb724fa09a82abd69f6e
tijesus <emmanueltijesu@gmail.com>
2023-10-27 13:13:09 -0700

    addition of multiple numbers

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+/**
+ * main - entry point
+ * @argc: counter parameter
+ * @argv: vector parameter
+ * Return: 0 success
+ */
+int main(int argc, char *argv[])
+{
+	int i, j;
+	int add = 0;
+
+
+	for (i = 1; i < argc; i++)
+	{
+		for (j = 0; j < argv[i][j]; j++)
+		{
+			if (!isdigit(argv[i][j]))
+			{
+				printf("Error");
+				return (1);
+			}
+		}
+		add += atoi(argv[i]);
+	}
+	printf("%d\n", add);
+	return (0);
+}
