17e5bc17c498994124f7d233c6630c2c10ae66d1
tijesus <emmanueltijesu@gmail.com>
2023-10-27 11:38:00 -0700

    return the count for the arguement

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - to return total number of an arguement
+ * @argc: counter parameter
+ * @argv: vector parameter
+ * Return: 0 success
+ */
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+
+	printf("%d\n", argc - 1);
+	return (0);
+}
