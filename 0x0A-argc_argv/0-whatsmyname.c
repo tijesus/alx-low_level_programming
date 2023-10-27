89eaee31db4302ece72af808eccf374fce91a104
tijesus <emmanueltijesu@gmail.com>
2023-10-27 11:22:56 -0700

    print file name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - function to print program name
+ * @argc: paramenter for counter
+ * @argv: parameter for vector
+ * Return: 0 success
+ */
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
