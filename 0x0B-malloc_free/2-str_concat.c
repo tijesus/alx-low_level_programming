b255fc31760a45079484b83f4c11bb0691f5dfef
tijesus <emmanueltijesu@gmail.com>
2023-10-31 09:56:28 -0700

    concatenate two strings

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -19,6 +19,10 @@ char *str_concat(char *s1, char *s2)
 		i++;
 		len++;
 	}
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 	ptr = (char *)(malloc((len + 1) * sizeof(char)));
 
 	if (ptr == NULL)
