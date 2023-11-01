a13710694bb53641f89254669a8fccbbeebe0fee
tijesus <emmanueltijesu@gmail.com>
2023-11-01 01:44:25 +0100

    return 2 dimentional array

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -1,46 +1,36 @@
 #include "main.h"
-#include <stdlib.h>
 /**
- * alloc_grid - Return a pointer to a 2 dimensional array of integers.
- * @width: Columns.
- * @height: Rows.
- * Return: Pointer to a 2 dimensional array.
+ * alloc_grid - function to return pointer to a 2 dimensional array
+ * @width: row array
+ * @height: column array
+ * Return: 0 success
  */
 int **alloc_grid(int width, int height)
 {
+	int **ptr;
 	int i, j;
 
-	int **arrd2;
 
 	if (width <= 0 || height <= 0)
 		return (NULL);
+	ptr = (int **)malloc(height * sizeof(int *));
 
-	/*Create a pointer to pointer rows*/
-	arrd2 = (int **) malloc(height * sizeof(int *));
-
-	if (arrd2 == NULL)
+	if (ptr == NULL)
 		return (NULL);
-
-	/*For each row create a pointer to each element*/
 	for (i = 0; i < height; i++)
 	{
-		arrd2[i] = (int *)malloc(width * sizeof(int));
-
-		if (arrd2[i] == NULL)
+		ptr[i] = (int *)malloc(width * sizeof(int));
+		if (ptr[i] == NULL)
 		{
-			for (; i >= 0; i--)
-				free(arrd2[i]);
-			free(arrd2);
+			for (j = 0; j < i; j++)
+			{
+				free(ptr[j]);
+			}
+			free(ptr);
 			return (NULL);
 		}
-	}
-
-	/*Fill the array*/
-	for (i = 0; i < height; i++)
-	{
 		for (j = 0; j < width; j++)
-			arrd2[i][j] = 0;
+			ptr[i][j] = 0;
 	}
-
-	return (arrd2);
+	return (ptr);
 }
