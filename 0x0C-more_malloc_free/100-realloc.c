49ba0eb6fd7dda9443f2b2f01e92e4a6270c46ad
tijesus <emmanueltijesu@gmail.com>
2023-11-03 02:22:58 +0100

    reallocation of memory block

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -9,24 +9,30 @@
 */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-	unsigned char *new_ptr;
+	unsigned char *new_ptr, *old_ptr = ptr;
+	unsigned int i;
 
-	if (new_size == old_size)
+	if (ptr == NULL)
+	{
+		new_ptr = malloc(new_size);
+		return (new_ptr);
+	}
+	else if (new_size == old_size)
 		return (ptr);
-	if (new_size == 0 && ptr == NULL)
+	else if (new_size == 0 && ptr == NULL)
 	{
 		free(ptr);
 		return (NULL);
 	}
 
-	new_ptr = (void *)realloc(ptr, new_size);
+	new_ptr = malloc(new_size);
 	if (new_ptr == NULL)
 		return (NULL);
 
 	if (new_size > old_size)
-		memcpy(new_ptr, ptr, old_size);
-	else
-		memcpy(new_ptr, ptr, new_size);
+		new_size = old_size;
+	for(i = 0; i < new_size; i++)
+		new_ptr[i] = old_ptr[i];
 	free(ptr);
 	return (new_ptr);
 }
