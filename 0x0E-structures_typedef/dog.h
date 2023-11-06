2e78c208133dd55fdb163885432c2f0c024f50ec
tijesus <emmanueltijesu@gmail.com>
2023-11-06 17:28:13 +0100

    created structure for dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+/**
+ * struct dog - structure for dog
+ * @name: dog;s name
+ * @age: dog's age
+ * @owner: owner of the dog
+*/
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
