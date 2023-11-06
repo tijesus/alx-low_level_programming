7d0f5648455ce2adbd568bb07568857a0d27855f
tijesus <emmanueltijesu@gmail.com>
2023-11-06 20:16:47 +0100

    printing dog struct variable

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -12,7 +12,9 @@ typedef struct dog
 	char *name;
 	float age;
 	char *owner;
-}dog_t;
+}dog;
+
+typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
