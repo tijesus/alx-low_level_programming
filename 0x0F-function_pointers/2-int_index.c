f880157746fba977d9451dad4c043e70382a076c
tijesus <emmanueltijesu@gmail.com>
2023-11-08 21:19:28 +0100

    checking integer

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -10,12 +10,12 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
-	if (array == NULL || cmp == NULL)
+	if (size <= 0 || array == NULL || cmp == NULL)
 		return (-1);
 	for (i = 0; i < size; i++)
 	{
 		if (cmp(array[i]) != 0)
 			return (i);
 	}
-	return (-i);
+	return (-1);
 }
---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,56 @@
+#include "3-calc.h"
+
+/**
+ * op_add - function to add
+ * @a: first value
+ * @b: secod value
+ * Return: 0 success
+*/
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - function to add
+ * @a: first value
+ * @b: secod value
+ * Return: 0 success
+*/
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - function to add
+ * @a: first value
+ * @b: secod value
+ * Return: 0 success
+*/
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - function to add
+ * @a: first value
+ * @b: secod value
+ * Return: 0 success
+*/
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - function to add
+ * @a: first value
+ * @b: secod value
+ * Return: 0 success
+*/
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
