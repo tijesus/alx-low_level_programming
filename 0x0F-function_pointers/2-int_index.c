aae142370c450ebc5ea56f6294e84e273792a0ae
tijesus <emmanueltijesu@gmail.com>
2023-11-08 19:03:57 +0100

    compare if its integer

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -5,24 +5,26 @@
  * @array: array to function
  * @size: size of function
  * @cmp: comparing pointer to a function
+ * Return: cmp success
 */
 int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
 	if (size <= -1 || array == NULL || cmp == NULL)
+	{
 		return (-1);
-	
+	}
 	for (i = 0; i < size; i++)
-		{
-			if (cmp(array[i]))
-				return (i);
-			if (isdigit(cmp(array[i])) == 0 )
-				cmp(array[i]);
-			else if ((cmp(array[i])) < 0)
-				cmp(array[i]);
-			else
-				return(-1);
-		}
-	return(cmp(array[i]));
+	{
+		if (cmp(array[i]))
+			return (i);
+		if (isdigit(cmp(array[i])) == 0)
+			cmp(array[i]);
+		else if ((cmp(array[i])) < 0)
+			cmp(array[i]);
+		else
+			return (-1);
+	}
+	return (cmp(array[i]));
 }
