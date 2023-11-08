483c7f4f32a2fb20016858b9c221889dd4844bc8
tijesus <emmanueltijesu@gmail.com>
2023-11-08 19:32:26 +0100

    search for integers

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,5 +1,4 @@
 #include "function_pointers.h"
-#include <ctype.h>
 /**
  * int_index - check if its integer
  * @array: array to function
@@ -12,9 +11,7 @@ int int_index(int *array, int size, int (*cmp)(int))
 	int i;
 
 	if (size <= 0 || array == NULL || cmp == NULL)
-	{
 		return (-1);
-	}
 	for (i = 0; i < size; i++)
 	{
 		if (cmp(array[i]) != 0)
