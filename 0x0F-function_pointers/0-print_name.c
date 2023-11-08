47579c945e72c171094630af0e3af0d8c9912d45
tijesus <emmanueltijesu@gmail.com>
2023-11-08 16:28:23 +0100

    print name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -2,10 +2,11 @@
 
 /**
  * print_name - function to print name
- * @name: name to print 
+ * @name: name to print
  * @f: pointer to another function
 */
 void print_name(char *name, void (*f)(char *))
 {
-	f(name);
+	if (name != NULL && f != NULL)
+		f(name);
 }
