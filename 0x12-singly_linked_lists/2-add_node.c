f67f2bb203a59c90af063dcdbc9fec637dbbd1e0
tijesus <emmanueltijesu@gmail.com>
2023-11-16 20:10:32 +0100

    create new node at the beginning of a list

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+ * add_node - creating new node
+ * @head: first node
+ * @str: pointer to str
+ * Return: 0 success
+*/
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new_node;
+
+	new_node = malloc(sizeof(list_t));
+	if (new_node == NULL)
+		return (NULL);
+	new_node->str = strdup(str);
+	new_node->len = strlen(str);
+	new_node->next = *head;
+	*head = new_node;
+	return (*head);
+}
