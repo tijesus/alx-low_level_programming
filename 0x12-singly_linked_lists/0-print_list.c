10ae40bbcd3b1ab4f6fef2549d51c312fab76a32
tijesus <emmanueltijesu@gmail.com>
2023-11-16 18:44:22 +0100

    print element of a list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+/**
+ * print_list - print list of linked list
+ * @h: head pointer
+ * Return: unsigned int
+*/
+size_t print_list(const list_t *h)
+{
+	size_t element;
+
+	element = 0;
+	if (h != NULL)
+	{
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%d] %s\n", h->len, h->str);
+		h = h->next;
+		element++;
+	}
+	return (element);
+}
