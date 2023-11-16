2fe102cb5c32f398c8fc6a4139fb3f8b5110d176
tijesus <emmanueltijesu@gmail.com>
2023-11-16 20:43:49 +0100

    create new node to the end of a list

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+/**
+ * add_node_end - creating node at the end of a list
+ * @head: pointer to head
+ * @str: pointer to string
+ * Return: 0 success
+*/
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *new_node, *temp;
+
+	new_node = malloc(sizeof(list_t));
+	if (new_node == NULL)
+		return (NULL);
+	new_node->str = strdup(str);
+	new_node->len = strlen(str);
+	new_node->next = NULL;
+	temp = *head;
+	if (temp == NULL)
+		*head = new_node;
+	else
+	{
+		while (temp->next != NULL)
+			temp = temp->next;
+		temp->next = new_node;
+	}
+	return (*head);
+}
