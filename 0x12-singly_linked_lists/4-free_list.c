a609df9af956984220a3d42ea815712a34ff8d80
tijesus <emmanueltijesu@gmail.com>
2023-11-16 21:14:22 +0100

    free list

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -5,13 +5,13 @@
 */
 void free_list(list_t *head)
 {
-    list_t *t = NULL;
-    
-    for (t = head; head != NULL;)
-    {
-        t = head->next;
-        free(head->str);
-        free(head);
-        head = t;
-    }
+	list_t *t = NULL;
+
+	for (t = head; head != NULL;)
+	{
+		t = head->next;
+		free(head->str);
+		free(head);
+		head = t;
+	}
 }
