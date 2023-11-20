50144b254565303dfbee772bd882e7b53cb47f0b
tijesus <emmanueltijesu@gmail.com>
2023-11-20 19:24:38 +0100

    add node to eginning of list

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -8,12 +8,12 @@
 listint_t *add_nodeint(listint_t **head, const int n)
 {
 	listint_t *newNode;
-	
+
 	newNode = ((listint_t *)malloc(sizeof(listint_t)));
 	if (newNode == NULL)
 		return (NULL);
 
-	newNode->n = n; 
+	newNode->n = n;
 	newNode->next = *head;
 	*head = newNode;
 	return (newNode);
