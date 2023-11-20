c98082d4f2d378a3c1f5969a2fa979628e97b446
tijesus <emmanueltijesu@gmail.com>
2023-11-20 19:23:39 +0100

    add node to eginning of list

---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+ * add_nodeint - adding nodde at the beginning of a list
+ * @head: pointer to the address of head
+ * @n: valueto be added to the list
+ * Return: new node value
+*/
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *newNode;
+	
+	newNode = ((listint_t *)malloc(sizeof(listint_t)));
+	if (newNode == NULL)
+		return (NULL);
+
+	newNode->n = n; 
+	newNode->next = *head;
+	*head = newNode;
+	return (newNode);
+}
