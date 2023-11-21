842ae8a9d58bc0c41681fa0e48f0af838a29a602
tijesus <emmanueltijesu@gmail.com>
2023-11-21 22:28:39 +0100

    insertng node in a position

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -8,5 +8,23 @@
 */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
+	listint_t *newNode, *temp;
 
+	temp = *head;
+	if (temp == NULL)
+		return (0);
+
+	newNode = malloc(sizeof(listint_t));
+	if (newNode == NULL)
+		return (NULL);
+
+	newNode->n = n;
+	while (idx != 1)
+	{
+		temp = temp->next;
+		idx--;
+	}
+	newNode->next = temp->next;
+	temp->next = newNode;
+	return (newNode);
 }
