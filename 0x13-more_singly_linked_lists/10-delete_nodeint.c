a6ecbaab4e044a029ff8d15152db693a0da173c5
tijesus <emmanueltijesu@gmail.com>
2023-11-21 23:32:55 +0100

    delete node at a position

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -7,5 +7,31 @@
 */
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
+	listint_t *curr, *prev;
+	size_t no;
 
+	if (*head == NULL)
+		return (-1);
+
+	if (index == 0)
+	{
+		*head = (*head)->next;
+		free(curr);
+		return (1);
+	}
+
+	prev = *head;
+	curr = (*head)->next;
+	for (no = 1; curr && no < index; no++)
+	{
+		prev = curr;
+		curr = curr->next;
+	}
+
+	if (curr == NULL)
+		return (-1);
+
+	prev->next = curr->next;
+	free(curr);
+	return (1);
 }
