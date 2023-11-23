fdbce7d4ea80756ed2f3c1eb699e0f66e3c9bc41
tijesus <emmanueltijesu@gmail.com>
2023-11-23 23:03:44 +0100

    print the index of a value

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * get_bit - get bit at a position
+ * @n: number
+ * @index: position to get bit
+ * Return: index or -1
+*/
+int get_bit(unsigned long int n, unsigned int index)
+{
+	unsigned int i;
+
+	/* condition for index exceeding bit after converting from 
+	* byte to bit
+	*/
+	if (index > sizeof(size_t) * 8)
+		return (-1);
+	for (i = 0; i < index; i++)
+		n = n >> 1;
+	return ((n & 1));
+}
