68bc231580c74c289f8d1d6496d6aac6ee00a095
tijesus <emmanueltijesu@gmail.com>
2023-11-27 22:47:55 +0100

    append text into a file

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -14,11 +14,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	file_des = open(filename, O_WRONLY | O_APPEND);
 	if (file_des == -1)
 		return (-1);
-	if (text_content == NULL)
-	{
-		close(file_des);
-		return (-1);
-	}
+
 	if (text_content != NULL)
 	{
 		for (count_text = 0; text_content[count_text]; count_text++)
