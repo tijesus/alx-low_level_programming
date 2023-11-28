b8bbbd6a1440d6a6e25c93a27a31c1273f4e9ace
tijesus <emmanueltijesu@gmail.com>
2023-11-28 01:18:16 +0100

    copy from one file to another

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,6 +1,6 @@
 #include "main.h"
 
-void handle_error(int init_file, int after_file, char **argv);
+void handle_error(int file_from, int file_to, char **argv);
 /**
  * main - entry point;
  * @argc: count number of arguement
@@ -9,7 +9,7 @@ void handle_error(int init_file, int after_file, char **argv);
 */
 int main(int argc, char **argv)
 {
-	int init_file, after_file, end_err;
+	int file_from, file_to, end_err;
 	ssize_t no_read = 1024, no_write;
 	char temp_holder[1024];
 
@@ -18,30 +18,30 @@ int main(int argc, char **argv)
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-	init_file = open(argv[1], O_RDONLY);
-	after_file = open(argv[2], O_WRONLY | O_TRUNC | O_CREAT | O_APPEND, 0664);
-	handle_error(init_file, after_file, argv);
+	file_from = open(argv[1], O_RDONLY);
+	file_to = open(argv[2], O_WRONLY | O_TRUNC | O_CREAT | O_APPEND, 0664);
+	handle_error(file_from, file_to, argv);
 
 	/*check the number of character to copy*/
 	while (no_read == 1024)
 	{
-		no_read = read(init_file, temp_holder, 1024);
+		no_read = read(file_from, temp_holder, 1024);
 		if (no_read == -1)
 			handle_error(-1, 0, argv);
-		no_write = write(after_file, temp_holder, 1024);
+		no_write = write(file_to, temp_holder, no_read);
 		if (no_write == -1)
 			handle_error(0, -1, argv);
 	}
-	end_err = close(init_file);
+	end_err = close(file_from);
 	if (end_err == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", init_file);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
 		exit(100);
 	}
-	end_err = close(after_file);
+	end_err = close(file_to);
 		if (end_err == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", init_file);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
 		exit(100);
 	}
 	return (0);
@@ -49,20 +49,20 @@ int main(int argc, char **argv)
 
 /**
  * handle_error - function to handle errors
- * @init_file: first file in the command line
- * @after_file: seccond file in the command line
+ * @file_from: first file in the command line
+ * @file_to: seccond file in the command line
  * @argv: pointer to handle aregument passed to command line
 */
-void handle_error(int init_file, int after_file, char *argv[])
+void handle_error(int file_from, int file_to, char **argv)
 {
-	if (init_file == -1)
+	if (file_from == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", *argv);
 		exit(98);
 	}
-	if (after_file == -1)
+	if (file_to == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[2]);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", (*argv + 1));
 		exit(99);
 	}
 }
