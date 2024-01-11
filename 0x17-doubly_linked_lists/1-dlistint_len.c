4a9f5477e11dd65c00e1406c508c70ab65068f88
tijesus <emmanueltijesu@gmail.com>
2024-01-11 09:50:09 -0800

    return number of node

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -4,12 +4,19 @@
  * @h: pointer to head
  * Return: the number of node
  */
-size_t print_dlistint(const dlistint_t *h)
+
+size_t dlistint_len(const dlistint_t *h)
 {
-	size_t n_nodes = 0;
+	const dlistint_t *temp;
+	size_t count = 0;
 
-	for (; h; h = h->next)
-		n_nodes++;
-
-	return (n_nodes);
+	temp = h;
+	while (h != NULL)
+		h = h->prev;
+	while (temp != NULL)
+	{
+		temp = temp->next;
+		count++;
+	}
+	return (count);
 }
