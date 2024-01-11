2a41b14b2eee4cdbcfc0ee2f24d152d816f4f32e
tijesus <emmanueltijesu@gmail.com>
2024-01-11 07:55:19 -0800

    print out the value of a linked list

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+/**
+ * print_dlistint - print out the value of a linked list
+ * @h: pointer to head
+ * Return: the number of node
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	const dlistint_t *temp;
+	size_t count = 0;
+
+	temp = h;
+	while (h != NULL)
+		h = h->prev;
+	while (temp != NULL)
+	{
+		printf("%d\n", temp->n);
+		temp = temp->next;
+		count++;
+	}
+	return (count);
+}
