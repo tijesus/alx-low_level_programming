1107e4ed41b30134adb0d4f7c99ff986175dd280
tijesus <emmanueltijesu@gmail.com>
2024-01-11 22:59:51 +0100

    free allocated node

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+/**
+ * free_dlistint - freeing memory allocated 
+ * @head: pointer to the address to free
+*/
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *temp;
+
+	while (head != NULL)
+	{
+		temp = head->next;
+		free(head);
+		head = temp;
+	}
+}
