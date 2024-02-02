6234c1a489c0e73fab676cd1299eb9c480f651f0
tijesus <emmanueltijesu@gmail.com>
2024-02-02 18:36:33 +0100

    retriving the value of a hash table

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -25,7 +25,6 @@ typedef struct hash_node_s
  * @size: size of the array
  * @array: pointer to the pointer of the first node of
  * array of a size
- *
 */
 typedef struct hash_table_s
 {
@@ -38,5 +37,6 @@ hash_table_t *hash_table_create(unsigned long int size);
 unsigned long int hash_djb2(const unsigned char *str);
 unsigned long int key_index(const unsigned char *key, unsigned long int size);
 int hash_table_set(hash_table_t *ht, const char *key, const char *value);
+char *hash_table_get(const hash_table_t *ht, const char *key);
 
 #endif /*HASH_TABLES*/
