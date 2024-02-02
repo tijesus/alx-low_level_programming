95cb4e2a371079d1016e00f8733e8279ef5823e0
tijesus <emmanueltijesu@gmail.com>
2024-02-02 20:45:35 +0100

    setting up a hash node

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,82 +1,49 @@
 #include "hash_tables.h"
-#include "string.h"
-
 /**
- * add_el - Add a new node at the beginning of a hash_node list.
- * @head: Pointer to pointer head of a hash_node list.
- * @c_value: Copy of string value of the element.
- * @key: Key of the element.
- * Return: The address of the new element, or NULL if it failed.
- */
-hash_node_t *add_el(hash_node_t **head, char *c_value, const char *key)
-{
-	hash_node_t *new;/*New node*/
-
-	/*Allocate memory for a new node*/
-	new = malloc(sizeof(hash_node_t));
-	if (new == NULL)
-	{
-		free(c_value);
-		return (NULL);
-	}
-
-	/*Create a new node*/
-	new->value = c_value;
-	new->key = strdup(key);
-	if (!new->key)
-	{
-		free(c_value);
-		free(new);
-		return (NULL);
-	}
-
-	new->next = *head;
-	*head = new;
-
-	return (new);
-}
-
-/**
- * hash_table_set - Adds an element to the hash table.
- * @ht: Head of hash table.
- * @key: Key of the element.
- * @value: Value of the element.
- * Return: 1 if it succeeded, 0 otherwise.
- */
+ * hash_table_set - setting up a hash table with key and value
+ * @ht: the hash table
+ * @key: key yo the hash table
+ * @value: value associated to the key
+ * Return: 1 on success and 0 on failure
+*/
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	unsigned long int index; /* Index of hash tables*/
-	hash_node_t *element;
-	char *c_value;
+	hash_node_t *new_node;
+	char *new_value;
+	unsigned long int position, i;
 
-	if (!ht || !key || *key == '\0' || !value)
+	if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
 		return (0);
 
-	/*Copy value*/
-	c_value = strdup(value);
-	if (!c_value)
+	new_value = strdup(value);
+	if (new_value == NULL)
 		return (0);
-
-	/*Find index*/
-	index = key_index((const unsigned char *)key, ht->size);
-
-	element = ht->array[index];
-
-	/*If key exists -> Update*/
-	for (; element; element = element->next)
+	position = key_index((const unsigned char *)key, ht->size);
+	for (i = position; ht->array[i]; i++)
 	{
-		if (strcmp(element->key, key) == 0)
+		if (strcmp(ht->array[i]->key, key) == 0)
 		{
-			free(element->value);
-			element->value = c_value;
+			free(ht->array[i]->value);
+			ht->array[i]->value = new_value;
 			return (1);
 		}
 	}
 
-	/*If key doesn't exist -> Add element*/
-	element = add_el(&(ht->array[index]), c_value, key);
-	if (!element)
+	new_node = malloc(sizeof(hash_node_t));
+	if (new_node == NULL)
+	{
+		free(new_value);
 		return (0);
+	}
+	new_node->key = strdup(key);
+	if (new_node->key == NULL)
+	{
+		free(new_value);
+		return (0);
+	}
+	new_node->value = new_value;
+	new_node->next = ht->array[position];
+	ht->array[position] = new_node;
 
 	return (1);
 }
