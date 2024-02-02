b0784d1152887e6fffef11b9d430dca7e8925fea
tijesus <emmanueltijesu@gmail.com>
2024-02-02 19:33:46 +0100

    create hash table

---
+++ 0x1A-hash_tables/hash_tables.h
@@ -0,0 +1,38 @@
+#ifndef HASH_TABLES_H
+#define HASH_TABLES_H
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdint.h>
+
+/**
+ * struct hash_node_s - Node for the hash table
+ * @key: the key to the hash table
+ * @value: the value to the key
+ * @next: the next node of the list
+*/
+typedef struct hash_node_s
+{
+	char *key;
+	char *value;
+	struct hash_node_s *next;
+} hash_node_t;
+
+
+/**
+ * struct hash_table_s - hash table structure
+ * @size: size of the array
+ * @array: pointer to the pointer of the first node of
+ * array of a size
+*/
+typedef struct hash_table_s
+{
+	int size;
+	hash_node_t **array;
+} hash_table_t;
+
+
+hash_table_t *hash_table_create(unsigned long int size);
+
+#endif
