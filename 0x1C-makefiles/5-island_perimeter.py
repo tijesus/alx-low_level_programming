973367a32672e6f25fb9abdce69c7fc5edc54d5d
tijesus <emmanueltijesu@gmail.com>
2024-03-21 00:27:22 +0100

    executing compilation without variable for RM

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+def island_perimeter(grid):
+    """return the perimeter of sn island"""
+	f;
+    
+
+if __name__ == "__main__":
+    grid = [
+        [0, 0, 0, 0, 0, 0],
+        [0, 1, 0, 0, 0, 0],
+        [0, 1, 0, 0, 0, 0],
+        [0, 1, 1, 1, 0, 0],
+        [0, 0, 0, 0, 0, 0]
+    ]
+    print(island_perimeter(grid))
