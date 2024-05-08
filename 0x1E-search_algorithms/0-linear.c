dad7d5aab60901f1a9addf35cad438ebfcf8f4d1
tijesus <emmanueltijesu@gmail.com>
2024-05-08 21:37:52 +0100

    linear search algorithm

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -1,11 +1,13 @@
 #include "search_algos.h"
+
 /**
- * @linear_search - linear search algorithm
- * @array - pointer to the array to search from
- * @sze - size of the array
- * @value - the value to search for
+ * linear_search - linear search algorithm
+ * @array: - pointer to the array to search from
+ * @size: - size of the array
+ * @value: - the value to search for
  * Return: first index where size is located in the array
 */
+
 int linear_search(int *array, size_t size, int value)
 {
 	size_t i;
